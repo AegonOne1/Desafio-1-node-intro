e0067d1e4094bc40526c4430a9b9287841b6513c
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-05-31 23:44:40 -0400

    importar y exportar

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const {registrar, leer} = require('./operaciones.js')
+const { registrar, leer } = require('./operaciones');
 
-const [operacion, nombre, edad, animal, color, enfermedad] = process.argv.slice
 
-if(operacion === "registrar"){
-    registrar(nombre, edad, animal, color, enfermedad)
+const [,, operacion, nombre, edad, animal, color, enfermedad] = process.argv;
+
+
+if (operacion === 'registrar') {
+    registrar(nombre, edad, animal, color, enfermedad);
 }
 
-if(operacion === "leer"){
-    leer()
+if (operacion === 'leer') {
+    leer();
 }
-    
--- operaciones.js
+++ operaciones.js
@@ -2,22 +2,30 @@
 const fs = require('fs');
 
 const registrar = (nombre, edad, animal, color, enfermedad) => {
-    const NuevoRegistro = {
-        nombre,
-        edad,
-        animal,
-        color,
-        enfermedad
-    };
 
-    fs.writeFileSync('./citas.json', JSON.stringify(2))
+    const data = fs.readFileSync('./citas.json', 'utf8');
+
+    const citas = JSON.parse(data);
+
+    const nuevoRegistro = { nombre, edad, animal, color, enfermedad };
+
+    citas.push(nuevoRegistro);
+
+    fs.writeFileSync('./citas.json', JSON.stringify(citas, null, 2));
+    console.log(nuevoRegistro);
+}
+
+const leer = () => {
+
+    const data = fs.readFileSync('./citas.json', 'utf8');
+
+    const citas = JSON.parse(data);
+
+    console.log(citas);
 }
 
 
-
-const leer = () =>{
-    fs.readFileSync('./citas.json', 'utf8')
-}
+module.exports = { registrar, leer };
 
 
 
@@ -31,4 +39,5 @@ const leer = () =>{
 
 
 
-module.exports = {registrar, leer}
+
+module.exports = { registrar, leer }
